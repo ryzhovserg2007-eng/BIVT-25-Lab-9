93d6bdb17a7cc4ba0a4850460e289af1ecd3547c
ryzhovserg2007-eng <ryzhov.serg.2007@gmail.com>
2026-04-18 13:47:45 +0300

    Update White.cs

--- Lab9/White/White.cs
+++ Lab9/White/White.cs
@@ -1,3 +1,20 @@
-﻿namespace Lab9.White
+namespace Lab9.White
 {
+    public abstract class White
+    {
+        public string Input { get; private set; }
+
+        protected White(string input)
+        {
+            Input = input;
+        }
+    
+        public abstract void Review();
+
+        public virtual void ChangeText(string text)
+        {
+            Input = text;
+            Review();
+        }
+    }
 }
